134a5e733a370e2b59bb81f173e9d00819f4c2d6
Flavio <flavio@datacratic.com>
2014-11-19 11:38:52 -0500

    fix openRtb objects - seat bid was missing a key
    other fixes to openRtb response objects
    
    add missing ext field to response
    
    fix external-id type from str to uint
    
    added crid field to bid
    
    reading config from json file

--- rtbkit/examples/httpapi/http_bid_agent_ex.py
+++ rtbkit/examples/httpapi/http_bid_agent_ex.py
@@ -31,10 +31,15 @@ import json
 
 
 class openRtb_response():
-    """this is a helper class to build basic json objects"""
+    """this is a helper class to build basic OpenRTB json objects"""
 
-    # field names
+    # field names - constants to avoid magic strings inside the function
     key_id = "id"
+    key_bid = "bid"
+    key_crid = "crid"
+    key_ext = "ext"
+    key_extid = "external-id"
+    key_priority = "priority"
     key_impid = "impid"
     key_price = "price"
     key_seatbid = "seatbid"
@@ -42,13 +47,14 @@ class openRtb_response():
     # template obejcts
     bid_object = {key_id: "1",
                   key_impid: "1",
-                  key_price: 1.0}
-    seat_bid_object = [deepcopy(bid_object)]
+                  key_price: 1.0,
+                  key_crid: "",
+                  key_ext: {key_priority: 1.0}}
+    seat_bid_object = {key_bid: [deepcopy(bid_object)]}
     bid_response_object = {key_id: "1",
-                           key_seatbid: [deepcopy(seat_bid_object)]
+                           key_seatbid: [deepcopy(seat_bid_object)]}
 
     def get_empty_response(self):
-
         """returns an object with the scafold of an rtb response
         but containing only default values"""
         empty_resp = deepcopy(self.bid_response_object)
@@ -56,7 +62,8 @@ class openRtb_response():
         return empty_resp
 
     def get_default_response(self, req):
-    """returns an object with the scafold of an rtb response and fills some fields based on the request provided"""
+        """returns an object with the scafold of an rtb response
+        and fills some fields based on the request provided"""
 
         default_resp = None
 
@@ -67,28 +74,37 @@ class openRtb_response():
             # copy request id
             default_resp[self.key_id] = req[self.key_id]
 
-            # empty the seatbid list
-            default_resp[self.key_seatbid] = []
+            # empty the bid list (we assume only one seat bid for simplicity)
+            default_resp[self.key_seatbid][0][self.key_bid] = []
 
             # default values for some of the fields of the bid response
             id_counter = 0
-            new_seatbid = deepcopy(self.bid_object)
+            new_bid = deepcopy(self.bid_object)
 
             # iterate over impressions array from request and
-            # populate seatbid list
+            # populate bid list
             for imp in req["imp"]:
-            # -> imp is the field name @ the req
+                # -> imp is the field name @ the req
 
                 # dumb bid id, just a counter
                 id_counter = id_counter + 1
-                new_seatbid[self.key_id] = id_counter
+                new_bid[self.key_id] = str(id_counter)
 
                 # copy impression id as imp for this bid
-                new_seatbid[self.key_impid] = imp[self.key_id]
+                new_bid[self.key_impid] = imp[self.key_id]
+
+                externalId = 0
+                # copy external id to the response
+                try:
+                    externalId = imp[self.key_ext]["external-ids"][0]
+                    new_bid[self.key_ext][self.key_extid] = externalId
+                except:
+                    externalId = -1  # and do not add this fiel
 
                 # will keep the defaul price as it'll be changed by bidder
                 # and append this bid into the bid response
-                default_resp[self.key_seatbid].append(deepcopy(new_seatbid))
+                ref2bidList = default_resp[self.key_seatbid][0][self.key_bid]
+                ref2bidList.append(deepcopy(new_bid))
 
         return default_resp
 
@@ -98,8 +114,8 @@ class openRtb_response():
         # the request is accroding to the spec
         valid = True
         return valid
-    
-  
+
+
 class FixedPriceBidderMixIn():
     """Dumb bid agent Mixin that bid 100% at $1"""
 
@@ -109,12 +125,17 @@ class FixedPriceBidderMixIn():
 
     bid_config = None
     openRtb = openRtb_response()
-    
+
     def do_config(self):
+        cfg = open("http_config.json")
+        data = json.load(cfg)
         self.bid_config = {}
-        self.bid_config["probability"] = 1.0
-        self.bid_config["price"] = 1
-    
+        self.bid_config["probability"] = data["bidProbability"]
+        # self.bid_config["probability"] = 1.0
+        self.bid_config["price"] = 1.0
+        self.bid_config["creatives"] = data["creatives"]
+        # self.bid_config["creatives"] = [1]
+
     def do_bid(self, req):
         # -------------------
         # bid logic
@@ -122,8 +143,19 @@ class FixedPriceBidderMixIn():
         # just mapping the request to the response
         # and using the default price ($1) will do the work.
         # -------------------
+
+        # get defaul response
         resp = self.openRtb.get_default_response(req)
-        
+
+        # update bid with price and creatives
+        crndx = 0
+        ref2seatbid0 = resp[openRtb_response.key_seatbid][0]
+        for bid in ref2seatbid0[openRtb_response.key_bid]:
+            bid[openRtb_response.key_price] = self.bid_config["price"]
+            creativeId = str(self.bid_config["creatives"][crndx]["id"])
+            bid[openRtb_response.key_crid] = creativeId
+            crndx = (crndx + 1) % len(self.bid_config["creatives"])
+
         return resp
 
 
@@ -171,7 +203,7 @@ class HTTPBaseBidAgentRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
             try:
                 # process JSON if fails treat as exception
                 req = self.decode_json(lineStr)
-                
+
             except:
                 # can't bid if request message is not properly formated
                 print("can't bid if request message is not properly formated")
@@ -206,13 +238,15 @@ class HTTPBaseBidAgentRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         """ converts the HTML body String into object"""
         ss = StringIO(jsonStr)
         ret = json.load(ss)
-        print("json decoded")
+        print("json decoded ------------------------------------")
+        print(jsonStr)
         return ret
 
     def encode_json(self, jsonObj):
         """ converts an oject into a string to use as response"""
         ret = json.dumps(jsonObj)
-        print("json encoded")
+        print("json encoded ------------------------------------")
+        print(ret)
         return ret
 
     def process_bid(self, req):
@@ -220,7 +254,7 @@ class HTTPBaseBidAgentRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         resp = None
         print("got response")
         return resp
-    
+
     def http200_response(self, length):
         """ return valid headers"""
         self.send_response(200)
@@ -237,7 +271,7 @@ class HTTPBaseBidAgentRequestHandler(BaseHTTPServer.BaseHTTPRequestHandler):
         self.send_header("Content-Length", "0")
         self.end_headers()
 
-        
+
 class HTTPFixPriceBidAgentRequestHandler(HTTPBaseBidAgentRequestHandler,
                                          FixedPriceBidderMixIn):
     """
