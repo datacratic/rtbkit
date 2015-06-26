ce76117ff7d1aedd54c729ac17ea67abda1bba64
jsbejeau <jsbejeau@datacratic.com>
2015-06-26 14:14:48 -0400

    Fix build

--- rtbkit/core/agent_configuration/testing/rtb_agent_configuration_testing.mk
+++ rtbkit/core/agent_configuration/testing/rtb_agent_configuration_testing.mk
@@ -1,4 +1,4 @@
 # RTB Agent Configuration testing makefile
 # JS Bejeau, 25 June 2015
 
-$(eval $(call test,rtb_agent_config_validator_test,rtb_router ,boost))
+$(eval $(call test,rtb_agent_config_validator_test,rtb_router,boost))
